4da8bcc05b29c507f31891b0cab07d4c41e5c579
PGS-SOFT\kstepnikowski <kstepnikowski@pgs-soft.com>
2019-06-14 08:09:22 +0200

    Added new project about Shell PoC.

---
+++ XFFeatures/ShellApp/ShellApp/Views/ProfilePage.xaml.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace ShellApp.Views
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class ProfilePage : ContentPage
+    {
+        public ProfilePage()
+        {
+            InitializeComponent();
+        }
+
+        private void ImageButton_Clicked(object sender, EventArgs e)
+        {
+            Shell.Current.FlyoutIsPresented = true;
+        }
+    }
+}
