1af0df0129852786402c31a6b9b697ca984c3b5d
PGS-SOFT\kstepnikowski <kstepnikowski@pgs-soft.com>
2019-06-19 08:32:23 +0200

    Created WeatherPage.

---
+++ XFFeatures/ShellApp/ShellApp/Views/WeatherPage.xaml.cs
@@ -0,0 +1,15 @@
+﻿using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace ShellApp.Views
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class WeatherPage : ContentPage
+    {
+        public WeatherPage()
+        {
+            InitializeComponent();
+            WeatherForecastList.ItemSelected += (obj, sender) => WeatherForecastList.SelectedItem = null;
+        }
+    }
+}
